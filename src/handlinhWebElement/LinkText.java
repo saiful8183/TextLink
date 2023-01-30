f6071a957d0c9a41ad574f5391471d8ba3ea4a6e
saiful8183 <Manha103019@gmail.com>
2023-01-30 03:40:00 -0500

    My 33 project

---
+++ src/handlinhWebElement/LinkText.java
@@ -0,0 +1,39 @@
+package handlinhWebElement;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class LinkText {
+
+	public static void main(String[] args) {
+System.setProperty("webdriver.gecko.driver","C:\\Users\\Saiful Chowdhury\\OneDrive\\Desktop\\Driver\\geckodriver.exe");
+		
+		WebDriver driver=new FirefoxDriver();
+		
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+
+		
+		
+		WebElement glink=driver.findElement(By.linkText("Gmail"));
+		
+		boolean status=glink.isDisplayed();
+	System.out.println("It is displayed:"+status);
+	status= glink.isEnabled();
+	System.out.println("It is enabled:"+status);
+		
+	String name=glink.getText();
+	System.out.println("Text name is :"+name);
+		
+		
+	System.out.println(driver.getTitle());
+	
+	System.out.println(driver.getCurrentUrl());
+	
+	driver.close();
+	
+	}
+
+}
